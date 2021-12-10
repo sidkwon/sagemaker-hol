10c36ba49f0f53b8669e4d948df3fa5dc88fece5
annakie <annakie.kwon@gmail.com>
2021-12-10 02:45:50 +0000

    Completed 02.debugger

--- 02.debugger/mnist_keras_tf.py
+++ 02.debugger/mnist_keras_tf.py
@@ -31,26 +31,13 @@ if __name__ == '__main__':
     training_dir   = args.training
     validation_dir = args.validation
     
-    x_train = np.load(os.path.join(training_dir, 'training.npz'))['image']
-    y_train = np.load(os.path.join(training_dir, 'training.npz'))['label']
-    x_val  = np.load(os.path.join(validation_dir, 'validation.npz'))['image']
-    y_val  = np.load(os.path.join(validation_dir, 'validation.npz'))['label']
+    x_train = np.load(os.path.join(training_dir, 'x_train.npy'))
+    y_train = np.load(os.path.join(training_dir, 'y_train.npy'))
+    x_val  = np.load(os.path.join(validation_dir, 'x_val.npy'))
+    y_val  = np.load(os.path.join(validation_dir, 'y_val.npy'))
     
     # input image dimensions
     img_rows, img_cols = 28, 28
-
-    # Tensorflow needs image channels last, e.g. (batch size, width, height, channels)
-    K.set_image_data_format('channels_last')  
-    print(K.image_data_format())
-
-    if K.image_data_format() == 'channels_first':
-        print("Incorrect configuration: Tensorflow needs channels_last")
-    else:
-        # channels last
-        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-        x_val = x_val.reshape(x_val.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
-        batch_norm_axis=-1
 
     print('x_train shape:', x_train.shape)
     print(x_train.shape[0], 'train samples')
@@ -64,39 +51,49 @@ if __name__ == '__main__':
     
     # Convert class vectors to binary class matrices
     num_classes = 10
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_val   = keras.utils.to_categorical(y_val, num_classes)
+#     y_train = keras.utils.to_categorical(y_train, num_classes)
+#     y_val   = keras.utils.to_categorical(y_val, num_classes)
     
-    model = Sequential()
+#     model = Sequential()
     
-    # 1st convolution block
-    model.add(Conv2D(64, kernel_size=(3,3), padding='same', input_shape=input_shape))
-    model.add(BatchNormalization(axis=batch_norm_axis))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=2))
+#     # 1st convolution block
+#     model.add(Conv2D(64, kernel_size=(3,3), padding='same', input_shape=input_shape))
+#     model.add(BatchNormalization(axis=batch_norm_axis))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2,2), strides=2))
     
-    # 2nd convolution block
-    model.add(Conv2D(128, kernel_size=(3,3), padding='valid'))
-    model.add(BatchNormalization(axis=batch_norm_axis))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=2))
+#     # 2nd convolution block
+#     model.add(Conv2D(128, kernel_size=(3,3), padding='valid'))
+#     model.add(BatchNormalization(axis=batch_norm_axis))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2,2), strides=2))
 
-    # Fully connected block
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.3))
+#     # Fully connected block
+#     model.add(Flatten())
+#     model.add(Dense(512))
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.3))
 
-    # Output layer
-    model.add(Dense(num_classes, activation='softmax'))
+#     # Output layer
+#     model.add(Dense(num_classes, activation='softmax'))
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
     
     print(model.summary())
 
     if gpu_count > 1:
         model = multi_gpu_model(model, gpus=gpu_count)
         
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True),
+#     model.compile(loss=keras.losses.categorical_crossentropy,
+#                   optimizer=SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True),
+#                   metrics=['accuracy', 'mse'])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     
     model.fit(x_train, y_train, batch_size=batch_size,
@@ -108,11 +105,13 @@ if __name__ == '__main__':
     print('Validation loss    :', score[0])
     print('Validation accuracy:', score[1])
     
-    # save Keras model for Tensorflow Serving
-    sess = K.get_session()
-    tf.saved_model.simple_save(
-        sess,
-        os.path.join(model_dir, 'model/1'),
-        inputs={'inputs': model.input},
-        outputs={t.name: t for t in model.outputs})
+    model.save(args.model_dir + '/1')
+    
+#     # save Keras model for Tensorflow Serving
+#     sess = K.get_session()
+#     tf.saved_model.simple_save(
+#         sess,
+#         os.path.join(model_dir, 'model/1'),
+#         inputs={'inputs': model.input},
+#         outputs={t.name: t for t in model.outputs})
     
