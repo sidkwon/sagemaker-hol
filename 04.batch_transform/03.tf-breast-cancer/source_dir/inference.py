aec48848339c900f6e5d61c80c6ebf291f9662b3
Sid Kwon <annakie.kwon@gmail.com>
2022-01-04 17:00:36 +0000

    20210105-01

---
+++ 04.batch_transform/03.tf-breast-cancer/source_dir/inference.py
@@ -0,0 +1,40 @@
+import json
+
+def input_handler(data, context):
+    """ Pre-process request input before it is sent to TensorFlow Serving REST API
+    Args:
+        data (obj): the request data, in format of dict or string
+        context (Context): an object containing request and configuration details
+    Returns:
+        (dict): a JSON-serializable dict that contains request body and headers
+    """
+    if context.request_content_type == 'text/csv':
+        request = data.read().decode('utf-8').rstrip('\n')
+        request = [float(x) for x in request.split(',')]
+        request.pop(0) # Remove "ID" column
+        
+        return json.dumps({
+            'instances': [request]
+        })
+
+    raise ValueError('{{"error": "unsupported content type {}"}}'.format(
+        context.request_content_type or "unknown"))
+
+
+def output_handler(data, context):
+    """Post-process TensorFlow Serving output before it is returned to the client.
+    Args:
+        data (obj): the TensorFlow serving response
+        context (Context): an object containing request and configuration details
+    Returns:
+        (bytes, string): data to return to client, response content type
+    """
+    if data.status_code != 200:
+        raise ValueError(data.content.decode('utf-8'))
+
+    response_content_type = context.accept_header
+    
+    prediction = json.loads(data.content.decode("utf-8"))['predictions'][0][0]
+    output = json.dumps({'predictions': prediction})
+
+    return output, response_content_type
---
+++ 04.batch_transform/03.tf-breast-cancer/source_dir/tf_batch.py
@@ -0,0 +1,84 @@
+import argparse
+import numpy as np
+import os
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, BatchNormalization
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser()
+
+    # 사용자가 전달한 하이퍼 파라미터를 command-line argument로 전달받아 사용함
+    parser.add_argument('--epochs', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--learning_rate', type=float, default=0.1)
+    
+    # data directories
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    parser.add_argument('--validation', type=str, default=os.environ.get('SM_CHANNEL_VALIDATION'))
+    
+    # model directory: we will use the default set by SageMaker, /opt/ml/model
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    
+    return parser.parse_known_args()
+
+    
+def get_train_data(train_dir):
+    
+    x_train = np.load(os.path.join(train_dir, 'data_train_x.npy'))
+    y_train = np.load(os.path.join(train_dir, 'data_train_y.npy'))
+    print('x_train', x_train.shape,'y_train', y_train.shape)
+
+    return x_train, y_train
+
+
+def get_validation_data(validation_dir):
+    
+    x_validation = np.load(os.path.join(validation_dir, 'data_val_x.npy'))
+    y_validation = np.load(os.path.join(validation_dir, 'data_val_y.npy'))
+    print('x_validation', x_validation.shape,'y_validation', y_validation.shape)
+
+    return x_validation, y_validation
+
+if __name__ == "__main__":
+    args, _ = parse_args()
+    
+    x_train, y_train = get_train_data(args.train)
+    x_validation, y_validation = get_validation_data(args.validation)
+    
+    device = '/cpu:0' 
+    print(device)
+    batch_size = args.batch_size
+    epochs = args.epochs
+    learning_rate = args.learning_rate
+    print('batch_size = {}, epochs = {}, learning rate = {}'.format(batch_size, epochs, learning_rate))
+
+    with tf.device(device):
+        model = tf.keras.Sequential([
+            # input layer
+            tf.keras.layers.Dense(30, input_shape=(30,), activation='relu'),
+            tf.keras.layers.Dense(15, activation='relu'),
+            tf.keras.layers.Dense(10,activation = 'relu'),
+            # we use sigmoid for binary output
+            # output layer
+            tf.keras.layers.Dense(1, activation='sigmoid')
+            ]
+        )
+
+        model.summary()
+#         optimizer = tf.keras.optimizers.SGD(learning_rate)
+        model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy', 'mse'])    
+        model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
+                  validation_data=(x_validation, y_validation))
+
+        # evaluate on test set
+        scores = model.evaluate(x_validation, y_validation, batch_size, verbose=2)
+        print("\nTest MSE :", scores)
+        
+        model.save(args.model_dir + '/1')
